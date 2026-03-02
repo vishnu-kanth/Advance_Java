06daa131b14261f75066a4db2e0c971d936d59f6
Y.Vishnu Kanth <114687297+vishnu-kanth@users.noreply.github.com>
2026-03-02 15:28:33 +0530

    Delete javaday1 directory

--- javaday1/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args){
-//       double n = 9.0;
-//       double m = 6.6;
-//       double result=n+m;
-//        char c= '8';
-       System.out.println(c);
-    }
-}
